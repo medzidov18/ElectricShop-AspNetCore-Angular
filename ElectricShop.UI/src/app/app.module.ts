5aa6c3f893e258919793e0d0995b117eccf1d641
Elvin Medzidov <medzidov18.03@gmail.com>
2022-08-12 16:25:30 +0400

    добавил компоненты и сервисы в ангуляр, написал несколько функций и соединил ангуляр с веб апи

--- ElectricShop.UI/src/app/app.module.ts
+++ ElectricShop.UI/src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DeviceComponent } from './device/device.component';
+import { ShowDeviceComponent } from './device/show-device/show-device.component';
+import { AddEditDeviceComponent } from './device/add-edit-device/add-edit-device.component';
+import { DeviceApiService } from './device-api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceComponent,
+    ShowDeviceComponent,
+    AddEditDeviceComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [DeviceApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
