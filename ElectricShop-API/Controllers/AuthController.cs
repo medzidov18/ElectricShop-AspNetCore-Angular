2bdf035d1d084e428765b63f627b7052d521ade4
Elvin Medzidov <medzidov18.03@gmail.com>
2022-08-29 21:32:12 +0400

    написал на беке апм для аутентификации администраторов, провёл все миграции и создал приложение ангуляр для него

---
+++ ElectricShop-API/Controllers/AuthController.cs
@@ -0,0 +1,132 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using ElectricShop_API.Data;
+using ElectricShop_API.Services.AdminUserService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ElectricShop_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IAdminUserService _adminUserService;
+        private readonly DataContext _context;
+
+        public AuthController(IConfiguration configuration, IAdminUserService adminUserService, DataContext context)
+        {
+            _configuration = configuration;
+            _adminUserService = adminUserService;
+            _context = context;
+        }
+
+        [HttpGet, Authorize]
+        public ActionResult<string> GetMe()
+        {
+            var userName = _adminUserService.GetMyName();
+            return Ok(userName);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AdminUser>> Register(AdminUserDto request)
+        {
+            AdminUser adminUser = new AdminUser();
+            CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            adminUser.Username = request.Username;
+            adminUser.PasswordHash = passwordHash;
+            adminUser.PasswordSalt = passwordSalt;
+
+            _context.AdminUsers.Add(adminUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(adminUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<string>> Login(AdminUserDto request)
+        {
+            var adminUser = await _context.AdminUsers.FindAsync(request.Username);
+            if (adminUser.Username != request.Username)
+            {
+                return BadRequest("User not found.");
+            }
+
+            if (!VerifyPasswordHash(request.Password, adminUser.PasswordHash, adminUser.PasswordSalt))
+            {
+                return BadRequest("Wrong password.");
+            }
+
+            string token = CreateToken(adminUser);
+
+            return Ok(token);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAdminUser(string userName)
+        {
+            var adminUser = _context.AdminUsers.Find(userName);
+
+            if (_context.AdminUsers == null)
+            {
+                return NotFound();
+            }
+            if (adminUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.AdminUsers.Remove(adminUser);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private string CreateToken(AdminUser user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, "Admin")
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds);
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
+        }
+    }
+}
--- ElectricShop-API/Program.cs
+++ ElectricShop-API/Program.cs
@@ -1,20 +1,55 @@
+using System.Configuration;
+using System.Text;
+using ElectricShop_API;
 using ElectricShop_API.Data;
+using ElectricShop_API.Services.AdminUserService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IAdminUserService, AdminUserService>();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme (\"bearer {token}\")",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
 builder.Services.AddCors(options => options.AddPolicy(name: "ElectronicOrigins",
     policy =>
     {
-        policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+        policy.WithOrigins("http://localhost:4200", "http://localhost:4201").AllowAnyMethod().AllowAnyHeader();
     }));
 
 var app = builder.Build();
@@ -29,6 +64,8 @@ app.UseCors("ElectronicOrigins");
 
 app.UseHttpsRedirection();
 
+app.UseCookiePolicy();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
