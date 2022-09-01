cb81227a60af80a57162c90284847a2bb83e8cdd
Elvin Medzidov <medzidov18.03@gmail.com>
2022-09-01 15:26:57 +0400

    оработал панель админимстратора, теперь администраторы могут добавлять, изменять и удалять устройства

--- ElectericShopAdminPanel.UI/src/app/app-routing.module.ts
+++ ElectericShopAdminPanel.UI/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeviceComponent } from './components/device/device.component';
 import { ShowDeviceComponent } from './components/device/show-device/show-device.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
-    { path: "", component: ShowDeviceComponent,canActivate:[AuthGuard] },
+    { path: "", component: DeviceComponent,canActivate:[AuthGuard] },
     {path:"login",component:LoginComponent}
 ];
 
--- ElectericShopAdminPanel.UI/src/app/components/device/device.component.ts
+++ ElectericShopAdminPanel.UI/src/app/components/device/device.component.ts
@@ -1,79 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DeviceApiService } from 'src/app/services/device-api.service';
 import { IDevice } from 'src/app/models/device';
+import { AddEditDeviceComponent } from './add-edit-device/add-edit-device.component';
+import { ShowDeviceComponent } from './show-device/show-device.component';
 
 @Component({
   selector: 'app-device',
   templateUrl: './device.component.html',
-  styleUrls: ['./device.component.css']
-})
+  styleUrls: ['./device.component.css']})
 export class DeviceComponent implements OnInit {
-
-    title = 'ElectricShop.UI';
     devices: IDevice[] = []
-    categoryList$: Observable<any[]>;
-    categoryList: any[];
-  
-    categoryMap: Map<number, string> = new Map();
-  
+
     constructor(private service: DeviceApiService) { }
     
     ngOnInit(): void {
-      this.service.getDevicesList().subscribe(devices => {
-          this.devices = devices;
-          this.refreshCategoryMap();
-      })
+      
     }
 
     modalTitle: string = '';
     activateAddEditDeviceComponent: boolean = false;
-    device: any;
 
     laptopDevices() {
         this.service.getDevicesListWithOneCategory(2).subscribe(devices => {
             this.devices = devices;
-            this.refreshCategoryMap();
         })
     }
 
     phoneDevices() {
         this.service.getDevicesListWithOneCategory(1).subscribe(devices => {
             this.devices = devices;
-            this.refreshCategoryMap();
         })
-    }
-  
-    modalAdd() {
-      this.device = {
-          id: 0,
-          name: null,
-          image: null,
-          shortDescription: null,
-          fullDescription: null,
-          price: 0,
-          categoryId: 0
-      }
-      this.modalTitle = "Добавить устройство";
-      this.activateAddEditDeviceComponent = true;
-    }
-    
-    modalClose() {
-        this.activateAddEditDeviceComponent = false;
-        this.service.getDevicesList().subscribe(devices => {
-            this.devices = devices;
-            this.refreshCategoryMap();
-        })
-    }
-
-    refreshCategoryMap() {
-      this.service.getCategoryList().subscribe(data => {
-          this.categoryList = data;
-  
-          for (let i = 0; i < data.length; i++) 
-          {
-              this.categoryMap.set(this.categoryList[i].id, this.categoryList[i].name);
-          }
-      })
     }
 }
